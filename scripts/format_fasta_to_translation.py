474c367eef46a1e5fad95b33a586e393784108e6
Stanley Fujimoto <sfujimoto@gmail.com>
2015-12-14 16:49:51 -0700

    adding text parsing, formatting, and statistics scripts

---
+++ scripts/format_fasta_to_translation.py
@@ -0,0 +1,67 @@
+'''
+the purpose of this script is to convert a multi-entry fasta file into a file
+that is acceptable by the tensorflow translation program
+
+writen by stanley fujimoto
+11 dec 2015
+'''
+
+import sys, argparse
+
+def convert( args ):
+    input_path = args.input
+    output_path = args.output
+    word_size = args.word_size
+
+    sys.stderr.write( "Reading from:\t" + input_path + "\n" )
+    sys.stderr.write( "Writing to:\t" + output_path + "\n" )
+
+    count = 0
+    len_sum = 0
+    shortest = float( 'inf' )
+    longest = 0
+
+    with open( input_path ) as in_f:
+        with open( output_path, 'w' ) as out_f:
+            seq = ""
+            for line in in_f:
+                if line[ 0 ] == '>':
+                    if len( seq ) > 0:
+                        out_f.write( " ".join( seq[ x * word_size : x * word_size + word_size ] for x in xrange( len( seq ) / word_size ) ) + "\n" )
+
+                        if len( seq ) > longest:
+                            longest = len( seq )
+                        if len( seq ) < shortest:
+                            shortest = len( seq )
+                        len_sum += len( seq )
+
+                        seq = ""
+                        count += 1
+                else:
+                    seq += line.strip()
+
+    sys.stderr.write( "Finished writing " + str( count ) + " sequences\n" )
+    sys.stderr.write( "Longest sequence:\t" + str( longest ) + "\n" )
+    sys.stderr.write( "Shortest sequence:\t" + str( shortest ) + "\n" )
+    sys.stderr.write( "Average seq length:\t" + str( float( len_sum ) / count ) + "\n" )
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+            description = "Convert a multi-entry fasta file into a format recognized by the Tensorflow translation program",
+            )
+    parser.add_argument( 'word_size',
+            type = int,
+            help = "Size of the word to split string into, all sequences must be divisible by the word_size"
+            )
+    parser.add_argument( 'input',
+            type = str,
+            help = "Input fasta file"
+            )
+    parser.add_argument( 'output',
+            type = str,
+            help = "Output file path"
+            )
+
+    args = parser.parse_args()
+
+    convert( args )
---
+++ scripts/text_stats.py
@@ -0,0 +1,39 @@
+'''
+This script will provide basic statistics on a given text file.
+Each line is assumed to be a different sentence
+
+Written by Stanley Fujimoto
+'''
+
+import sys
+
+longest = 0
+longest_seq = ''
+shortest = float( 'inf' )
+cur_sum = 0
+count = 0
+
+with open( sys.argv[ 1 ] ) as fh:
+    for line in fh:
+        len_line = len( line.strip().split( ' ' ) )
+
+        if len_line > longest:
+            longest = len_line
+            longest_seq = line.strip()
+        if len_line < shortest:
+            shortest = len_line
+            shortest_seq = line.strip()
+        cur_sum += len_line
+        count += 1
+
+print( "# of seqs:\t" + str( count ) )
+print( "Longest length:\t" + str( longest ) )
+print( "Shortest length:\t" + str( shortest ) )
+print( "Average length:\t" + str( float( cur_sum ) / count ) )
+
+print( "longest:" )
+print( longest_seq )
+print( "*************")
+print( "shortest:" )
+print( shortest_seq )
+
