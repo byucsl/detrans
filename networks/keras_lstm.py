added model and model weights saving to each epoch, masking is now set, fixed the 0 padding issue

networks/keras_lstm.py
@@ -21,7 +21,9 @@ from keras.optimizers import SGD
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import one_hot, text_to_word_sequence
 from keras.layers.embeddings import Embedding
+import time
 
+model_name = time.strftime( "%Y-%m-%d" ) + "-" + time.strftime( "%H-%M-%S" )
 
 # set model params
 epoches = int( sys.argv[ 3 ] )
@@ -34,11 +36,11 @@ sys.stderr.write( "Reading data\n" )
 sys.stderr.write( "\tReading amino acid seqs file..." )
 raw_text = open( sys.argv[ 1 ] ).read().strip()
 aas_set = set( raw_text.split() )
-len_aas = len( aas_set ) 
-aa_indices = dict( ( c, i ) for i, c in enumerate( aas_set ) )
+len_aas = len( aas_set ) + 1
+aa_indices = dict( ( c, i + 1 ) for i, c in enumerate( aas_set ) )
 
-print "number of amino acids:\t", str( len_aas )
-print aas_set
+#print "number of amino acids:\t", str( len_aas )
+#print aas_set
 
 #for key, val in aa_indices.iteritems():
 #    print key, val
@@ -61,21 +63,21 @@ for s_idx, seq in enumerate( aas ):
     aas_p.append( seq_one_hot )
     #print len( seq.split() )
     #print len( seq_one_hot )
-    print zip( seq.split(), seq_one_hot )
+    #print zip( seq.split(), seq_one_hot )
     #for r_idx, res in enumerate( seq_one_hot ):
     #    aas_p[ s_idx, r_idx, res ] = 1
     #aas_p.append( text_to_word_sequence( seq, lower = False, split = " " ) )
-    print aas_p[ -1 ]
+    #print aas_p[ -1 ]
 aas_p = pad_sequences( aas_p, maxlen = maxlen )
-print "*" * 10
-print aas_p
+#print "*" * 10
+#print aas_p
 
 cds = []
 sys.stderr.write( "\tReading codon seqs file..." )
 raw_text = open( sys.argv[ 2 ] ).read().strip()
 cds_set = set( raw_text.split() )
-len_codons = len( cds_set )
-codon_indices = dict( ( c, i ) for i, c in enumerate( cds_set ) )
+len_codons = len( cds_set ) + 1
+codon_indices = dict( ( c, i + 1 ) for i, c in enumerate( cds_set ) )
 idx_to_codon = {v: k for k, v in codon_indices.items()}
 
 #for key, val in codon_indices.iteritems():
@@ -87,7 +89,7 @@ sys.stderr.write( " Done! (" + str( len( cds ) ) + " codon seqs read)]\n" )
 
 sys.stderr.write( "Maxlen:\t" + str( maxlen ) + "\n" )
 
-print cds
+#print cds
 cds_p = []
 cds_p = np.zeros( ( len( aas_p ), maxlen, len_codons ), np.bool )
 for sidx, seq in enumerate( cds ):
@@ -103,12 +105,12 @@ for sidx, seq in enumerate( cds ):
     #print zip( cds_t, cds_1 )
 #cds_p = pad_sequences( cds_p, maxlen = maxlen )
 #cds_p = np.zeros( ( len( aas_p ), maxlen, len_codons ), np.bool )
-print cds_p
+#print cds_p
 
-for i in range( len( cds_p ) ):
-    print "*" * 20
-    print len( cds_p[ i ] )
-    print len( aas_p[ i ] )
+#for i in range( len( cds_p ) ):
+#    print "*" * 20
+#    print len( cds_p[ i ] )
+#    print len( aas_p[ i ] )
 
 # pad the inputs for rnn usage
 #aas = pad_sequences( aas, maxlen = maxlen )
@@ -130,10 +132,10 @@ nb_classes = 10
 
 print('Build model...')
 model = Sequential()
-model.add(Embedding(len_aas, 128))
-model.add(LSTM(len_codons, return_sequences=True))
-model.add(TimeDistributedDense(len_codons))
-model.add(Activation('softmax'))
+model.add( Embedding( len_aas, 128, mask_zero = True ) )
+model.add( LSTM( len_codons, return_sequences = True ) )
+model.add( TimeDistributedDense( len_codons ) )
+model.add( Activation( 'softmax' ) )
 #model.add(Dense(1))
 #model.add(Activation('sigmoid'))
 
@@ -143,6 +145,12 @@ for iteration in range(1, epoches):
     print()
     print('-' * 50)
     print('Iteration', iteration)
+    
+    print( "Saving model..." )
+    json_string = model.to_json()
+    open( model_name + ".json", 'w' ).write( json_string )
+    model.save_weights( model_name + ".h5", overwrite = True )
+
     model.fit( train_x, train_y, batch_size=1, nb_epoch=1)
 
 results = model.predict( train_x )