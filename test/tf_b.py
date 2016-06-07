31525b6a00745da185a49df36c3bf1da184ece1f
masakistan <sfujimoto@gmail.com>
2016-06-06 18:26:18 -0600

    edited

--- test/tf_b.py
+++ test/tf_b.py
@@ -6,14 +6,22 @@ import random
 
 # globals
 lstm_size = 8
-number_of_layers = 2
+number_of_layers = 3
 batch_size = 5
 n_steps = 5 # number of time steps
 n_input = 3 # size of something?
-n_hidden = 8 # hidden layer num of features
+n_hidden = lstm_size # hidden layer num of features
 n_classes = 2 # MNIST total classes (0-9 digits)
 learning_rate = 0.001
 num_instances = 10
+
+def unpack_sequence(tensor):
+    """Split the single tensor of a sequence into a list of frames."""
+    return tf.unpack(tf.transpose(tensor, perm=[1, 0, 2]))
+
+def pack_sequence(sequence):
+    """Combine a list of the frames into a single tensor of the sequence."""
+    return tf.transpose(tf.pack(sequence), perm=[1, 0, 2])
 
 # fake test data
 data = [ [ [ float( random.getrandbits( 1 ) ) for x in range( n_input ) ] for x in range( n_steps ) ] for x in range( batch_size ) ]
@@ -47,7 +55,7 @@ biases = {
 weights = {
     # Hidden layer weights => 2*n_hidden because of foward + backward cells
     'hidden': tf.Variable(tf.random_normal([n_input, 2*n_hidden])),
-    'out': tf.Variable(tf.random_normal( [ 2 * 2 * n_hidden, n_classes ] ) )
+    'out': tf.Variable(tf.random_normal( [  ( lstm_size * 2 ) * number_of_layers, n_classes ] ) )
 }
 
 _x = tf.transpose( x, [ 1, 0, 2 ] )
@@ -62,12 +70,16 @@ outputs = rnn.bidirectional_rnn(
         stacked_lstm_fw,
         stacked_lstm_bw,
         _x,
+        #unpack_sequence( _x ),
+        #tf.unpack( tf.transpose( _x, perm = [ 1, 0, 2 ] ) ),
         initial_state_fw = initial_state_fw,
         initial_state_bw = initial_state_bw,
         sequence_length = early_stop
         )
 
 states = outputs[ -1 ]
+
+#packed_states = pack_sequence( states )
 
 pred = tf.matmul( outputs[ -1 ], weights[ 'out' ] ) + biases[ 'out' ]
 
@@ -126,16 +138,17 @@ with tf.Session() as sess:
                 )
         print "predictions"
         print predictions
-        outs = sess.run(
+        _states = sess.run(
                 states,
                 feed_dict = epoch_dict
                 )
         print "Iter " + str(step*batch_size) + ", Minibatch Loss= " + "{:.6f}".format(loss) + \
               ", Training Accuracy= " + "{:.5f}".format(acc)
-        print "outs"
-        for out in outs:
+        print _states.shape
+        print "*" * 20
+        for _state in _states:
             print "*" * 100
-            print out
+            print _state
         step += 1
 
 
